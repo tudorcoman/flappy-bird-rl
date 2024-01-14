a51eec669e22cd36bca8c3437ea31565c7fed30b
Tudor Coman <tudor.a.coman@gmail.com>
2024-01-14 19:50:47 +0200

    Make env playable

--- FlappyRL/main.py
+++ FlappyRL/main.py
@@ -17,6 +17,7 @@ bird_x = 50
 bird_y = 200
 bird_y_change = 0
 score = 0
+
 #function to create and show the bird
 def create_bird(x, y):
     screen.blit(bird_image, (x, y))
@@ -27,6 +28,7 @@ font = pygame.font.Font('freesansbold.ttf', 32)
 def display_score():
     text = font.render("Score: " + str(score), True, (255, 255, 255))
     screen.blit(text, (20, 20))
+
 #pipes
 x_location = 700
 x_location_change = 0
@@ -36,23 +38,17 @@ def create_pipe(height):
     screen.blit(pipe_up, (x_location, height)) 
     screen.blit(pipe_down, (x_location, height + 800))
 
-#colisions
-def check_collision(x_location, height, bird_y):
-    # print(x_location)                  
+#collisions
+def check_collision(x_location, height, bird_y):             
     pipe_width = 100
     bird_width = 93
     bird_x = 50
     if bird_x + bird_width >= x_location and bird_x <= x_location + pipe_width:
-        # print("bird y", bird_y)
-        # print("height", height)
-        # print("height + 614", height + 614)
-        # print("height + 750", height + 750)
 
         if bird_y <= height + 614 or bird_y >= height + 800 - 80:
             return True
 
     return False
-
 
 running = True
 jumping = False
@@ -60,10 +56,12 @@ last_time_jump = -1000
 jump_interval = 300
 last_animation_time = 0 
 animation_time = 0
-aniimation_interval = 300                   
+aniimation_interval = 1000 
+start_jump_y = 0
+max_jump_height = 50
+
 while running:
-    # display_score()
-    #show backround image while game is running
+    # show backround image while game is running
     screen.fill((0, 0, 0))
     screen.blit(background_image, (0,0))
     current_time = pygame.time.get_ticks()  # get current time
@@ -77,14 +75,22 @@ while running:
                 last_time_jump = current_time
                 last_animation_time = pygame.time.get_ticks()                      
                 print ("am apasat space")
-                bird_y_change = -0.2
                 x_location_change = -0.2
-                jumping = True
+                if not jumping:
+                    jumping = True
+                    start_jump_y = bird_y
+                    bird_y_change = -0.6  # Jumping upwards
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_SPACE:
-                bird_y_change = 0.2
+                jumping = False
 
-
+    if jumping:
+        if start_jump_y - bird_y > max_jump_height:
+            jumping = False  # Stop the jump if max height is reached
+        else:
+            bird_y += bird_y_change
+    else:
+        bird_y_change = 0.2  # Falling down when not jumping
 
     #bird bounders
     if bird_y <= 0: 
@@ -99,12 +105,9 @@ while running:
     animation_time = pygame.time.get_ticks()  
     if animation_time - last_animation_time > aniimation_interval:
         if bird_y_change < 0:
-            print("Tre sa cad")
             last_animation_time = animation_time
             bird_y_change = 0.2                 
-    # if jumping == True:
-    #     bird_y_change = 0.5
-            
+
     #pipe creation
     x_location += x_location_change
 
@@ -113,7 +116,6 @@ while running:
         x_location = 700
         height = random.randint(-600, -450)
         score += 1
-        print(score)
     create_pipe(height)
 
     collision = check_collision(x_location, height, bird_y)
